fab825cc2b6d40e71f12b88a6a90deac757eac18
Shaik Hatim <85576231+Hteam121@users.noreply.github.com>
2024-11-19 12:07:44 -0600

    initial commit with errors in CMSIS

---
+++ Debug/Drivers/CMSIS/Source/arm_rfft_fast_f32.d
@@ -0,0 +1,67 @@
+Drivers/CMSIS/Source/arm_rfft_fast_f32.o: \
+ ../Drivers/CMSIS/Source/arm_rfft_fast_f32.c \
+ ../Drivers/CMSIS/Source/arm_math.h \
+ ../Drivers/CMSIS/Source/arm_math_types.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Source/arm_math_memory.h \
+ ../Drivers/CMSIS/Source/dsp/none.h \
+ ../Drivers/CMSIS/Include/arm_math_types.h \
+ ../Drivers/CMSIS/Source/dsp/utils.h \
+ ../Drivers/CMSIS/Source/dsp/basic_math_functions.h \
+ ../Drivers/CMSIS/Include/arm_math_memory.h \
+ ../Drivers/CMSIS/Include/dsp/none.h ../Drivers/CMSIS/Include/dsp/utils.h \
+ ../Drivers/CMSIS/Source/dsp/interpolation_functions.h \
+ ../Drivers/CMSIS/Source/dsp/bayes_functions.h \
+ ../Drivers/CMSIS/Include/dsp/statistics_functions.h \
+ ../Drivers/CMSIS/Include/dsp/basic_math_functions.h \
+ ../Drivers/CMSIS/Include/dsp/fast_math_functions.h \
+ ../Drivers/CMSIS/Source/dsp/matrix_functions.h \
+ ../Drivers/CMSIS/Source/dsp/complex_math_functions.h \
+ ../Drivers/CMSIS/Source/dsp/statistics_functions.h \
+ ../Drivers/CMSIS/Source/dsp/controller_functions.h \
+ ../Drivers/CMSIS/Source/dsp/support_functions.h \
+ ../Drivers/CMSIS/Source/dsp/distance_functions.h \
+ ../Drivers/CMSIS/Include/dsp/matrix_functions.h \
+ ../Drivers/CMSIS/Source/dsp/svm_functions.h \
+ ../Drivers/CMSIS/Include/dsp/svm_defines.h \
+ ../Drivers/CMSIS/Source/dsp/fast_math_functions.h \
+ ../Drivers/CMSIS/Source/dsp/transform_functions.h \
+ ../Drivers/CMSIS/Include/dsp/complex_math_functions.h \
+ ../Drivers/CMSIS/Source/dsp/filtering_functions.h \
+ ../Drivers/CMSIS/Include/dsp/support_functions.h \
+ ../Drivers/CMSIS/Source/dsp/quaternion_math_functions.h \
+ ../Drivers/CMSIS/Source/dsp/window_functions.h
+../Drivers/CMSIS/Source/arm_math.h:
+../Drivers/CMSIS/Source/arm_math_types.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Source/arm_math_memory.h:
+../Drivers/CMSIS/Source/dsp/none.h:
+../Drivers/CMSIS/Include/arm_math_types.h:
+../Drivers/CMSIS/Source/dsp/utils.h:
+../Drivers/CMSIS/Source/dsp/basic_math_functions.h:
+../Drivers/CMSIS/Include/arm_math_memory.h:
+../Drivers/CMSIS/Include/dsp/none.h:
+../Drivers/CMSIS/Include/dsp/utils.h:
+../Drivers/CMSIS/Source/dsp/interpolation_functions.h:
+../Drivers/CMSIS/Source/dsp/bayes_functions.h:
+../Drivers/CMSIS/Include/dsp/statistics_functions.h:
+../Drivers/CMSIS/Include/dsp/basic_math_functions.h:
+../Drivers/CMSIS/Include/dsp/fast_math_functions.h:
+../Drivers/CMSIS/Source/dsp/matrix_functions.h:
+../Drivers/CMSIS/Source/dsp/complex_math_functions.h:
+../Drivers/CMSIS/Source/dsp/statistics_functions.h:
+../Drivers/CMSIS/Source/dsp/controller_functions.h:
+../Drivers/CMSIS/Source/dsp/support_functions.h:
+../Drivers/CMSIS/Source/dsp/distance_functions.h:
+../Drivers/CMSIS/Include/dsp/matrix_functions.h:
+../Drivers/CMSIS/Source/dsp/svm_functions.h:
+../Drivers/CMSIS/Include/dsp/svm_defines.h:
+../Drivers/CMSIS/Source/dsp/fast_math_functions.h:
+../Drivers/CMSIS/Source/dsp/transform_functions.h:
+../Drivers/CMSIS/Include/dsp/complex_math_functions.h:
+../Drivers/CMSIS/Source/dsp/filtering_functions.h:
+../Drivers/CMSIS/Include/dsp/support_functions.h:
+../Drivers/CMSIS/Source/dsp/quaternion_math_functions.h:
+../Drivers/CMSIS/Source/dsp/window_functions.h:
